4432c1cbe413812c2a2df0582def24e06cd88efc
Mariane Siqueira Machado <mariane.machado@gmail.com>
2014-04-16 23:42:25 +0530

    Improving readability of EightQueensSolver

--- src/ChessboardPrinter.java
+++ src/ChessboardPrinter.java
@@ -1,5 +1,5 @@
 public class ChessboardPrinter {
-    
+
     public void print(int[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid.length; j++) {
--- src/EightQueensSolver.java
+++ src/EightQueensSolver.java
@@ -2,7 +2,6 @@ public class EightQueensSolver{
 
     private final QueenProblemPositionValidator queenProblemPositionValidator;
 
-
     public EightQueensSolver() {
         queenProblemPositionValidator = new QueenProblemPositionValidator();
     }
@@ -12,12 +11,22 @@ public class EightQueensSolver{
 
         for (int row = 0; row < grid.length; row++) {
             if (queenProblemPositionValidator.isValid(grid, row, column)) {
-                grid[row][column] = 1;
-                if(findSolution(grid, column + 1) == true)
+                assignQueenToPosition(grid, column, row);
+                if (findSolution(grid, column + 1) == true) {
                     return true;
-                grid[row][column] = 0;
+                } else {
+                    removeQueenFromPosition(grid, column, row);
+                }
             }
         }
         return false;
+    }
+
+    private void removeQueenFromPosition(int[][] grid, int column, int row) {
+        grid[row][column] = 0;
+    }
+
+    private void assignQueenToPosition(int[][] grid, int column, int row) {
+        grid[row][column] = 1;
     }
 }
