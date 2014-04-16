09a3648af276a058552408a49b64e27f3aa8218d
Mariane Siqueira Machado <mariane.machado@gmail.com>
2014-04-16 23:12:30 +0530

    Extracting code that checks position validation to its own class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
-    private static final int NUMBER_OF_QUEENS = 4;
     private static final int BOARDSIZE = 4;
     private static final int[][] board = new int[BOARDSIZE][BOARDSIZE];
 
     public static void main(String argv[]) {
         EightQueensSolver eightQueensSolver = new EightQueensSolver();
-        if (eightQueensSolver.solve8Queen(board, 0)) {
+        if (eightQueensSolver.solve(board, 0)) {
             EightQueensSolver.printGrid(board);
         } else {
             System.out.println("No solution");
